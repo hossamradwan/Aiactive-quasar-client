da735b289bdb734f7c328859cc501047a888600e
marcoref3at <marco_refaat@hotmail.com>
2021-04-09 21:33:19 +0200

    Rename6

--- src/components/Shared/Lpr/Toolbar/Toolbar.vue
+++ src/components/Shared/Lpr/Toolbar/Toolbar.vue
@@ -84,7 +84,7 @@ export default {
     search: require("./Shared/Search").default,
     "date-time": require("./Shared/DateTime").default,
     "filters-dropdown": require("./Shared/FiltersDropdown").default,
-    "facial-options": require("./Shared/VMSOptions").default
+    "facial-options": require("./Shared/FacialOptions").default
   }
 };
 </script>
